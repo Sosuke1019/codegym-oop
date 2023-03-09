e5312ef922c34f1a8ea8cf97ef0a32d80de76cea
Sosuke1019 <kato.052wazime@gmail.com>
2023-03-09 18:08:36 +0900

    fix 01_main.py

--- itoop-python/base/_01/main.py
+++ itoop-python/base/_01/main.py
@@ -1,5 +1,5 @@
 def main():
-    play(1, 2)
+    play(0, 2)
 
 
 def play(left_hand: int, right_hand: int):
@@ -9,12 +9,38 @@ def play(left_hand: int, right_hand: int):
 
 def judge(left_hand: int, right_hand: int) -> int:
     # 勝敗を判定して結果（1:勝ち, 2:引き分け, 3:負け）を返却しよう
-    return 0
+    if left_hand == 0:  # Goo
+        if right_hand == 0:  # Goo
+            return 2
+        elif right_hand == 1:  # Tyoki
+            return 1
+        else:  # Paa
+            return 3
+    elif left_hand == 1:  # Tyoki
+        if right_hand == 0:  # Goo
+            return 3
+        elif right_hand == 1:  # Tyoki
+            return 2
+        else:  # Paa
+            return 1
+    else: # Paa
+        if right_hand == 0:  # Goo
+            return 1
+        elif right_hand == 1:  # Tyoki
+            return 3
+        else:  # Paa
+            return 2
 
 
 def show_result(result: int):
     # 結果をもとに勝敗を print を使って表示しよう
-    print("結果を表示")
+    if result == 1:
+        print("勝ち")
+    elif result == 2:
+        print("引き分け")
+    else:
+        print("負け")
+
 
 
 if __name__ == '__main__':
